f5e04e22180447bf1ba66cc0c8f33c77c52cab74
pacharapa <mewmewmheaw@hotmail.com>
2017-05-08 23:52:40 +0700

    test

---
+++ effects.js
@@ -0,0 +1,6 @@
+$("decument").ready(function() {
+	
+});(function(){
+    $("img.image1").delay(0).fadeIn(1000);
+    $("#div2").delay("fast").fadeIn();
+});
---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8" \>
+<!-- <link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet"> -->
+<link href="https://fonts.googleapis.com/css?family=Itim" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script language="javascript" type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js" ></script>
+<script language="javascript" type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- <link rel="stylesheet" href="css/_stysheet.css"> -->
+
+<head>
+    <script language="javascript" type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js" ></script>
+    <script src="effects.js"></script>
+    <link rel="stylesheet" type="text/css" href="./css/_stysheet.css">
+    <title></title>
+</head>
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+    <div class="navbar-header">
+    <a class="navbar-brand" href="#">Steve Jobs</a>
+    </div>
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <!--dropdown-->
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown 
+          <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+        </ul> 
+    </div><!-- /.navbar-collapse -->
+    </div>
+</nav>
+<div class="container">
+  <div class="row">
+  <div class="col-md-6">
+    <div class="">
+      <img src="Pictures/jobs.png"  class="img-responsive test">
+    </div>
+  </div>
+    <div class="col-md-6">
+        <div class="desc">
+        	<h2 class="test2"> Steve Jobs (สตีฟ จอบส์) </h2>
+          
+            <h4 >จากวิกิพีเดีย สารานุกรมเสรี</h4>
+              <p class="paragragh1">
+              สตีเวน พอล จอบส์ (อังกฤษ: Steven Paul Jobs) หรือที่รู้จักในชื่อ สตีฟ จอบส์ (อังกฤษ: Steve Jobs; 24 กุมภาพันธ์ ค.ศ. 1955 - 5 ตุลาคม ค.ศ. 2011) เป็นผู้นำธุรกิจและนักประดิษฐ์ชาวอเมริกัน ผู้ร่วมก่อตั้ง ประธาน อดีตประธานบริหารของแอปเปิลคอมพิวเตอร์ และยังเคยเป็นประธานบริหาร พิกซาร์แอนิเมชันสตูดิโอส์ และเป็นคณะกรรมการบริหารบริษัทเดอะวอลต์ดิสนีย์ใน ค.ศ. 2006 หลังดิสนีย์ซื้อกิจการพิกซาร์
+            </p>
+               
+            <p class="paragragh2">
+                เขาร่วมก่อตั้งแอปเปิลคอมพิวเตอร์กับสตีฟ วอซเนียก ใน ค.ศ. 1976 เป็นผู้มีส่วนช่วยทำให้แนวความคิดเรื่องคอมพิวเตอร์ส่วนบุคคลเป็นที่นิยมขึ้นมา ด้วยเครื่อง Apple II ต่อมา เขาเป็นผู้แรกที่มองเห็นศักยภาพทางการค้าของส่วนประสานงานผู้ใช้แบบกราฟิกส์และเม้าส์ ที่ถูกพัฒนาขึ้นในศูนย์วิจัยซีร็อกซ์พาร์ค ของบริษัทซีร็อกซ์ และได้มีการผนวกเทคโนโลยีเหล่านี้เข้าไว้ในเครื่องแมคอินทอช หลังพ่ายแพ้ในการแย่งชิงอำนาจกับคณะกรรมการบริหารใน ค.ศ. 1984  จอบส์ลาออกจากแอปเปิลและก่อตั้งเน็กซ์ บริษัทพัฒนาแพลตฟอร์มคอมพิวเตอร์ โดยเฉพาะในการศึกษาขั้นอุดมศึกษาและตลาดธุรกิจ การซื้อกิจการเน็กซ์ของแอปเปิลใน ค.ศ. 1996 ทำให้จอบส์กลับเข้าทำงานในบริษัทแอปเปิลที่เขาร่วมก่อตั้งขึ้นนั้น และเขารับหน้าที่ CEO ตั้งแต่ ค.ศ. 1997 ถึง 2011 จอบส์ยังเป็นประธานบริหาร และผู้บริหารระดับสูงของพิกซาร์แอนิเมชันสตูดิโอส์ ผู้นำด้านการผลิตภาพยนตร์แอนิเมชันด้วยคอมพิวเตอร์กราฟิกส์ ทั้งเป็นผู้ถือหุ้นรายใหญ่ที่ 50.1% กระทั่งบริษัทวอลต์ดิสนีย์ซื้อกิจการไปใน ค.ศ. 2006 จอบส์เป็นผู้ถือหุ้นมากที่สุดของดิสนีย์ที่ 7% และเป็นสมาชิกคณะกรรมการบริหารของดิสนีย์
+                <span>
+                    <span>
+                        
+                    </span>
+                </span>
+            </p>
+        </div>
+            <div class="col-md-4 col-sm-offset-4 img1" ><img src="Pictures/ipad2.jpg" class="img-responsive "></div>
+            <div class="col-md-4 img2"> <img  src="Pictures/iphone6s-plus.jpg" class="img-responsive  " ></div>
+    </div>
+</div><!-- /.container-fluid -->
+</div>
+<p class="footer"> COPYRIGHT  2016  I3GATEWAY  ALL  RIGHTS  RESERVED </p>
+
+<script >
+$(document).ready(function(){
+ //    $('.test').delay(0).fadeIn(2200);
+ // $('#b').delay(1200).fadeIn(2200);
+
+    $('.test').fadeIn(1000, function(){
+        // 
+        // alert();
+        $('.desc').fadeIn(1000, function() {
+            $('.img1').fadeIn(1000,function(){
+                $('.img2').fadeIn(1000, function() {
+                    
+                });
+
+
+            });
+
+
+            
+        });
+    });
+
+
+});
+</script>
+
+</body>
+</html>
